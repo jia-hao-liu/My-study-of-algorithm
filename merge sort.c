7d1b1b8524cc6d7ec9c34fc82352c94ae289e3f3
jia-hao-liu <32381123+jia-hao-liu@users.noreply.github.com>
2017-11-25 14:06:21 +0800

    归并排序
    加入了非递归版本

--- merge sort.c
+++ merge sort.c
@@ -1,9 +1,11 @@
-#include<stdio.h>
+#include<stdio.h>                  //分为递归和非递归版本
 #define MaxSize 100
 
-void merge(int arr[], int left, int right, int rightend);
+void Msort_nonrecursion(int arr[],int temparr[],int N,int length);
+void mergesort_nonrecur(int arr[],int N);
+void merge(int arr[],int temparr[],int left, int right, int rightend);
 void mergesort(int arr[], int N);
-void Msort(int arr[], int left, int right);
+void Msort(int arr[],int temp[],int left, int right);
 
 
 int main(void)
@@ -14,40 +16,67 @@ int main(void)
     puts("Please input your numbers:");
     for(i=0;i<N;i++)
         scanf("%d", &number[i]);
-    mergesort(number,N);
+    mergesort_nonrecur(number,N);
     for(i=0;i<N;i++)
         printf("%d ",number[i]);
 
     return 0;
 }
 
+void Msort_nonrecursion(int arr[],int temparr[],int N,int length)
+{
+    int i;
+    for(i=0;i<=N-2*length;i+=2*length)
+    {
+        merge(arr,temparr,i,i+length,i+2*length-1);
+    }
+    if(i+length<N)
+        merge(arr,temparr,i,i+length,N-1);
+    else                                            //这一步是否多余？No
+    {
+        for(int j=i;j<N;j++)
+            temparr[j]=arr[j];
+    }
+}
 
+void mergesort_nonrecur(int arr[],int N)
+{
+    int length=1;
+    int temp[N];
+    while(length<N)
+    {
+        Msort_nonrecursion(arr,temp,N,length);
+        length*=2;
+        Msort_nonrecursion(temp,arr,N,length);
+        length*=2;
+    }
+}
 
 
 void mergesort(int arr[], int N)
 {
-    Msort(arr,0,N-1);
+    int temp[N];
+    Msort(arr,temp,0,N-1);
 }
 
-void Msort(int arr[], int left, int right)
+void Msort(int arr[],int temp[], int left, int right)
 {
     int mid=(left+right)/2;
     if(left<right)
     {
-        Msort(arr, left, mid);
-        Msort(arr,mid+1, right);
-        merge(arr,left, mid+1,right);
+        Msort(arr, temp,left, mid);
+        Msort(arr,temp,mid+1, right);
+        merge(arr,temp,left, mid+1,right);
     }
 }
 
 
 
 
-void merge(int arr[], int left, int right, int rightend)
+void merge(int arr[],int temparr[], int left, int right, int rightend)
 {   int i;
     int elementnum=rightend-left+1;
     int L=left;
-    int temparr[rightend];
     int tmppos=left;
     int leftend=right-1;
 
@@ -68,3 +97,4 @@ void merge(int arr[], int left, int right, int rightend)
         arr[rightend]=temparr[rightend];
 
 }
+
